ff610ca2849af7d31702a5c3cf604d40b801d907
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-11-28 10:55:17 -0700

    commit tree b141f3cbc9052c010bc0c316291f310113dcfead

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function saturdayFun(activity="roller-skate") {
+  if (activity === String){
   return `This Saturday, I want to ${activity}!`
+  } else {
+    return `This Saturday, I want to ${activity}!`
+  }
 }
 
 let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
+  switch (activity){
+    case ("play with kids"):
+      return `This Monday, I will ${activity}.`
+    case ('sleep in'):
+      return `This Monday, I will ${activity}.`
+    case ('go see family'):
+      return `This Monday, I will ${activity}.`
+    default :
+      return `This Monday, I will ${activity}.`
+  }
 }
 
 let wrapAdjective = function(style="*") {
   return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+    if (style === String && adjective === String){
+      return`You are ${style}${adjective}${style}!`
+    }else{
+      return `You are ${style}${adjective}${style}!`}
+    }
   }
-}
+
